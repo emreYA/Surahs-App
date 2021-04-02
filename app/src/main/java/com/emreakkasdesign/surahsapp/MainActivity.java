99c157b7ed28c44d4b20d83890cb644b3bb89b4b
emreYA <emreYA@users.noreply.github.com>
2021-04-02 21:25:59 +0200

    begin of fragments 2

--- app/src/main/java/com/emreakkasdesign/surahsapp/MainActivity.java
+++ app/src/main/java/com/emreakkasdesign/surahsapp/MainActivity.java
@@ -1,14 +1,22 @@
 package com.emreakkasdesign.surahsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    ViewPager2 viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager2) findViewById(R.id.viewpager);
+
+        CategoryAdapter adapter = new CategoryAdapter(this);
+
+        viewPager.setAdapter(adapter);
+
     }
 }
